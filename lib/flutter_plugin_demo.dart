762f43dd62a7e6496e4e2f3202f83a9319d7a069
liusilong-mini <1157867422@qq.com>
2019-10-08 23:14:04 +0800

    init commit

--- lib/flutter_plugin_demo.dart
+++ lib/flutter_plugin_demo.dart
@@ -5,14 +5,35 @@ import 'package:flutter/services.dart';
 class FlutterPluginDemo {
   static const MethodChannel _channel =
       const MethodChannel('flutter_plugin_demo');
+  static const EventChannel _eventChannel =
+      EventChannel("flutter_plugin_event");
+
+  static StreamController<String> streamController = StreamController<String>.broadcast();
+
+
+  /// 问题是这里没有主动调用
+  FlutterPluginDemo() {
+    print('FLutter 构造方法');
+    _eventChannel.receiveBroadcastStream().listen((data) {
+      streamController.sink.add(data);
+    });
+  }
+
+  static dispose(){
+    streamController.close();
+  }
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
-  
-  static Future<void> showToast() async {
-    await _channel.invokeMethod("showToast");
-  }
 
+  /// [message] Toast 的内容
+  static Future<void> showToast({String message}) async {
+    Map<String, String> params = Map<String, String>();
+    params['name'] = 'Lili';
+    params['age'] = '20';
+    params['country'] = 'China';
+    await _channel.invokeMethod("showToast", params);
+  }
 }
